7b236bab4aa8d3430f26bcc87e1a58f743bcb304
Kaushik Kulkarni <kaushikcfd@gmail.com>
2022-09-30 14:06:31 -0500

    able to parse isl/set.h

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use clang;
-use clang::token::Token;
+use clang::token::{Token, TokenKind};
 use codegen::Scope;
-use hashbrown::HashMap;
+use hashbrown::{HashMap, HashSet};
 use lazy_static::lazy_static;
 use std::fs;
 use std::path::Path;
@@ -21,9 +21,15 @@ lazy_static! {
                        ("isl_map", "uintptr_t"),
                        ("isl_aff", "uintptr_t"),
                        ("const char*", "*const c_char"),]);
+    static ref ISL_TYPES: HashSet<&'static str> = HashSet::from(["isl_aff",
+                                                                 "isl_context",
+                                                                 "isl_basic_set",
+                                                                 "isl_set",
+                                                                 "isl_basic_map",
+                                                                 "isl_map"]);
 }
 
-fn compare_tuples(x: &(u32, u32), y: &(u32, u32)) -> std::cmp::Ordering {
+fn compare_tuples(x: &(usize, usize), y: &(usize, usize)) -> std::cmp::Ordering {
     if x.0 == y.0 && x.1 == y.1 {
         std::cmp::Ordering::Equal
     } else if (x.0 < y.0) || (x.0 == y.0 && x.1 < y.1) {
@@ -33,32 +39,34 @@ fn compare_tuples(x: &(u32, u32), y: &(u32, u32)) -> std::cmp::Ordering {
     }
 }
 
-fn get_tokens_sorted_by_occurence(tokens: Vec<Token>) -> (HashMap<(u32, u32), u32>, Vec<Token>) {
-    let mut loc_to_token: HashMap<(u32, u32), Token> = HashMap::new();
+fn get_tokens_sorted_by_occurence(tokens: Vec<Token>)
+                                  -> (HashMap<(usize, usize), usize>, Vec<Token>) {
+    let mut loc_to_token: HashMap<(usize, usize), Token> = HashMap::new();
     for token in tokens {
         let loc = token.get_location();
         let (_, src_line, src_column) = loc.get_presumed_location();
-        let key = (src_line, src_column);
+        let key = (src_line as usize, src_column as usize);
         loc_to_token.insert(key, token);
     }
 
-    let mut sorted_locations: Vec<(u32, u32)> = loc_to_token.clone().into_keys().collect();
+    let mut sorted_locations: Vec<(usize, usize)> = loc_to_token.clone().into_keys().collect();
     sorted_locations.sort_by(compare_tuples);
 
     let position_to_token: Vec<_> = sorted_locations.iter().map(|x| loc_to_token[x]).collect();
-    let mut loc_to_position: HashMap<(u32, u32), u32> = HashMap::new();
+    let mut loc_to_position: HashMap<(usize, usize), usize> = HashMap::new();
     for (i, loc) in sorted_locations.into_iter().enumerate() {
-        loc_to_position.insert(loc, i as u32);
+        loc_to_position.insert(loc, i as usize);
     }
 
     (loc_to_position, position_to_token)
 }
 
-fn get_start_end_locations(e: clang::Entity) -> ((u32, u32), (u32, u32)) {
+fn get_start_end_locations(e: clang::Entity) -> ((usize, usize), (usize, usize)) {
     let src_range = e.get_range().unwrap();
     let start_src_loc = src_range.get_start().get_presumed_location();
     let end_src_loc = src_range.get_end().get_presumed_location();
-    ((start_src_loc.1, start_src_loc.2), (end_src_loc.1, end_src_loc.2))
+    ((start_src_loc.1 as usize, start_src_loc.2 as usize),
+     (end_src_loc.1 as usize, end_src_loc.2 as usize))
 }
 
 fn implement_bindings(dst_t: &str, src_t: &str, _dst_file: &str, src_file: &str) {
@@ -92,24 +100,75 @@ fn implement_bindings(dst_t: &str, src_t: &str, _dst_file: &str, src_file: &str)
     let _dst_impl = scope.new_impl(dst_t);
 
     for func_decl in func_decls {
-        println!("{:#?}", func_decl);
+        println!("Traversing {}", func_decl.get_name().unwrap());
         let (start_loc, end_loc) = get_start_end_locations(func_decl);
         let (start_idx, end_idx) = (loc_to_idx[&start_loc], loc_to_idx[&end_loc]);
-
         assert!(start_idx <= end_idx);
 
-        for idx in start_idx - 1..(end_idx + 1) {
-            println!("{:#?}", idx_to_token[idx as usize]);
-        }
+        // {{{ parse __isl_null, __isl_give
 
-        // for child in func_decl.get_children() {
-        //     println!("{:#?}", child);
-        // }
-        // let parm_decls = func_decl.get_arguments().unwrap();
-        // for parm_decl in parm_decls {
-        //     println!("Parameter.get_type {:#?}", parm_decl.get_type());
-        //     println!("Parameter.get_name {:#?}", parm_decl.get_name());
-        // }
+        let (isl_null, isl_give) =
+            if idx_to_token[start_idx - 1].get_kind() == TokenKind::Punctuation {
+                (false, false)
+            } else {
+                let qualifier_1 = idx_to_token[start_idx - 1];
+                assert_eq!(qualifier_1.get_kind(), TokenKind::Identifier);
+                if qualifier_1.get_spelling() == "__isl_null" {
+                    assert!(idx_to_token[start_idx - 2].get_spelling() != "__isl_give");
+                    (true, false)
+                } else if qualifier_1.get_spelling() == "__isl_give" {
+                    assert!(idx_to_token[start_idx - 2].get_spelling() != "__isl_null");
+                    (false, true)
+                } else {
+                    (false, false)
+                }
+            };
+
+        // }}}
+
+        // {{{ parse __isl_export, __isl_constructor
+
+        let (is_constructor, is_exported) = if isl_give {
+            let qualifier1 = idx_to_token[start_idx - 2];
+            let qualifier2 = idx_to_token[start_idx - 3];
+            if qualifier1.get_kind() == TokenKind::Punctuation {
+                (false, false)
+            } else if qualifier1.get_spelling() == "__isl_export" {
+                assert_eq!(qualifier2.get_kind(), TokenKind::Punctuation);
+                (false, true)
+            } else {
+                assert_eq!(qualifier1.get_spelling(), "__isl_constructor");
+                assert_eq!(qualifier2.get_kind(), TokenKind::Punctuation);
+                (true, false)
+            }
+        } else if isl_null {
+            let qualifier = idx_to_token[start_idx - 2];
+            if qualifier.get_kind() == TokenKind::Punctuation {
+                (false, false)
+            } else {
+                assert_eq!(qualifier.get_spelling(), "__isl_export");
+                (false, true)
+            }
+        } else {
+            let qualifier = idx_to_token[start_idx - 1];
+            if qualifier.get_kind() == TokenKind::Punctuation {
+                (false, false)
+            } else {
+                assert_eq!(qualifier.get_spelling(), "__isl_export");
+                assert!((idx_to_token[start_idx - 2].get_kind() == TokenKind::Punctuation)
+                        | (idx_to_token[start_idx - 2].get_spelling() == "endif"));
+                (false, true)
+            }
+        };
+
+        // }}}
+
+        if is_exported | is_constructor {
+            println!("Will export {}", func_decl.get_name().unwrap());
+        }
+    }
+
+    if true {
         panic!("Dandey aur kaam baaki hai");
     }
 
@@ -119,7 +178,7 @@ fn implement_bindings(dst_t: &str, src_t: &str, _dst_file: &str, src_file: &str)
     drop_impl.impl_trait("Drop");
     drop_impl.new_fn("drop")
              .arg_mut_self()
-             .line(format!("unsafe {{ {}_free(self.ptr) }}", { src_t }));
+             .line(format!("unsafe {{ {}_free(self.ptr) }}", src_t));
 
     // }}}
     //
@@ -136,3 +195,5 @@ fn main() {
                        "src/bindings/bset.rs",
                        "isl/include/isl/set.h");
 }
+
+// vim:fdm=marker
